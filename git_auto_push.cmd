a032ea8f68ccaf41132493395b6e6a7bbefafd88
ersurajrajput <thsurajsinghrajput@gmail.com>
2025-09-20 15:57:46 +0530

    update

--- git_auto_push.cmd
+++ git_auto_push.cmd
@@ -1,16 +1,14 @@
 @echo off
-REM Get current date and time
-for /f "tokens=1-4 delims=/: " %%a in ("%date% %time%") do (
-    set datetime=%%a-%%b-%%c_%%d
-)
+REM Ask user for commit message
+set /p commitMessage=Enter commit message: 
 
 REM Go to your repository folder (optional)
 REM cd "C:\path\to\your\repo"
 
 REM Git commands
 git add .
-git commit -m "Commit at %datetime%"
+git commit -m "%commitMessage%"
 git push
 
-echo Done! Commit message was: Commit at %datetime%
+echo Done! Commit message was: %commitMessage%
 pause
