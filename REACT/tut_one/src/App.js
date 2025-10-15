26cb06b022fb9350431f014183e1b0a2236ce4ec
ersurajrajput <thsurajsinghrajput@gmail.com>
2025-10-16 02:24:44 +0530

    react

---
+++ REACT/tut_one/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import navBar from './components/navBar';
+
+
+
+function App() {
+  return (
+   navBar()
+ 
+  );
+}
+
+
+
+export default App;
